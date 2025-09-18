4927368f9a7387fce41abf4b7584df8589f9b889
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-09-18 22:58:19 +0200

    fix: define GKE cluster name variable in Terraform configuration

--- kubernetes_cluster/main.tf
+++ kubernetes_cluster/main.tf
@@ -3,7 +3,7 @@ data "google_client_config" "default" {}
 module "kubernetes_cluster" {
   source = "../terraform/modules/kubernetes_cluster"
 
-  gke_cluster_name      = module.google_kubernetes_engine.cluster_name
+  gke_cluster_name      = var.cluster_name
   argocd_domain         = "argocd.whispr.epitech-msc2026.me"
   argocd_namespace      = "argocd"
   argocd_admin_password = "" # If empty, a random one will be generated
---
+++ kubernetes_cluster/variables.tf
@@ -0,0 +1,5 @@
+variable "gke_cluster_name" {
+  description = "GKE Cluster Name"
+  default     = "whispr-cluster"
+  type        = string
+}
