d5cad9a363472f4b1af425e57ec79a22dddfcbe2
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-09-18 22:51:35 +0200

    refactor(outputs): rename outputs for ArgoCD to improve clarity

--- terraform/modules/kubernetes_cluster/outputs.tf
+++ terraform/modules/kubernetes_cluster/outputs.tf
@@ -2,7 +2,7 @@
 # OUTPUTS
 ####################################################################################################
 
-output "namespace" {
+output "argocd_namespace" {
   value = var.argocd_namespace
 }
 
@@ -15,13 +15,13 @@ data "kubernetes_secret" "argocd_initial_admin_secret" {
   }
 }
 
-output "admin_password" {
+output "argocd_admin_password" {
   description = "Mot de passe admin ArgoCD"
   value       = nonsensitive(data.kubernetes_secret.argocd_initial_admin_secret.data["password"])
   sensitive   = false
 }
 
-output "server_url" {
+output "argocd_server_url" {
   description = "URL du serveur ArgoCD"
   value       = "https://${var.argocd_domain}"
 }
