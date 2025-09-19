77cab3a5abd74ee491ed92da9849b42e5c487312
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-09-19 10:37:13 +0200

    fix: update Kubernetes variables names

--- kubernetes_cluster/variables.tf
+++ kubernetes_cluster/variables.tf
@@ -1,5 +1,20 @@
-variable "gke_cluster_name" {
+variable "cluster_name" {
   description = "GKE Cluster Name"
   default     = "whispr-cluster"
+  type        = string
+}
+
+variable "cluster_endpoint" {
+  description = "GKE Cluster Endpoint"
+  type        = string
+}
+
+variable "cluster_ca_certificate" {
+  description = "GKE Cluster CA Certificate"
+  type        = string
+}
+
+variable "cluster_endpoint" {
+  description = "GKE Cluster Endpoint"
   type        = string
 }
