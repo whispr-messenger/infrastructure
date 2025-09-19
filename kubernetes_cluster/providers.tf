2dd1fcfb385c92b6d4a3f2e527d5f59cc1c6fd47
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-09-19 16:49:36 +0200

    refactor: Move the google_client_config to the google_kubernetes_engine
    It will be simple to setup the google provider once and make all infrastructure operations in the first workspace then feed the outputs of this workspace to the downstream kubernetes_cluster through tfe_outputs datasource

--- kubernetes_cluster/providers.tf
+++ kubernetes_cluster/providers.tf
@@ -2,9 +2,9 @@
 # KUBERNETES PROVIDER
 ####################################################################################################
 provider "kubernetes" {
-  host                   = "https://${data.tfe_outputs.whispr_gke.cluster_endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.tfe_outputs.whispr_gke.cluster_ca_certificate)
+  host                   = "https://${data.tfe_outputs.whispr_gke.values.cluster_endpoint}"
+  token                  = data.tfe_outputs.whispr_gke.values.access_token
+  cluster_ca_certificate = base64decode(data.tfe_outputs.whispr_gke.values.cluster_ca_certificate)
 }
 
 ####################################################################################################
@@ -12,8 +12,8 @@ provider "kubernetes" {
 ####################################################################################################
 provider "helm" {
   kubernetes {
-    host                   = "https://${data.tfe_outputs.whispr_gke.cluster_endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.tfe_outputs.whispr_gke.cluster_ca_certificate)
+    host                   = "https://${data.tfe_outputs.whispr_gke.values.cluster_endpoint}"
+    token                  = data.tfe_outputs.whispr_gke.values.access_token
+    cluster_ca_certificate = base64decode(data.tfe_outputs.whispr_gke.values.cluster_ca_certificate)
   }
 }
