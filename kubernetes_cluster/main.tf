8d09ae2c04031d46b0033a5af8944deb8be7a450
Gabriel Lopez <gabriel.lopez@epitech.eu>
2025-09-20 12:58:24 +0200

    fix: Update token retrieval to use google_client_config for Kubernetes provider
    Trying to use the one generated from the other workspace may lead to expired token usage

--- kubernetes_cluster/main.tf
+++ kubernetes_cluster/main.tf
@@ -1,7 +1,11 @@
+# Retrieve outputs from the GKE workspace
 data "tfe_outputs" "whispr_gke" {
   organization = "glopez-personnal"
   workspace    = "whispr-google-kubernetes-engine"
 }
+
+# Retrieve a fresh authentication token as the one stored in the other workspace state file may be expired
+data "google_client_config" "default" {}
 
 module "kubernetes_cluster" {
   source = "git::https://github.com/whispr-messenger/infrastructure.git//terraform/modules/kubernetes_cluster?ref=main"
